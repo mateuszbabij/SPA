b4b6de41ba5b73904a632f5f64a015e8b7473836
Mateusz <babijmateusz94@gmail.com>
2022-02-22 09:33:11 +0100

    Added background to dropdown menu and jQuery library

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
+//background for dropdown menu
+document.getElementById("navbarButton").addEventListener("click",function(){
+        document.getElementById("drop-bg").classList.toggle("collapse-bg")
+});
+//end
 
