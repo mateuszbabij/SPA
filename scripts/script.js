3416f8abf8842de54022aaea5112d70effb88cd2
Mateusz <babijmateusz94@gmail.com>
2022-02-22 19:52:18 +0100

    Cards updated

--- scripts/script.js
+++ scripts/script.js
@@ -6,27 +6,34 @@ $("#navbarButton").on("click",function(){
 
 //wstawianie zabiegu do html
 const cards = function (data, i) {
-  $(".row").html(
-    `<div class="col-12 col-sm-6 col-md-4 justify-content-center">
-        <div class="card">
+  $(".row").append(
+    `<div class="col">
+        <div class="card h-100" >
                 <img src="../images/${data[i].image}.jpg" class="card-img-top" alt="..."/>
                 <div class="card-body d-grid">
                         <h5 class="card-title">${data[i].name}</h5>
-                        <p class="card-text">${data[i].desc}</p>
-                        <a href="#" class="btn btn-primary">Zarezweruj</a>
+                        <p class="card-text"><br>${data[i].desc}</p>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item"> <i class="fa-solid fa-hands"></i> Część ciała: ${data[i].area}</li>
+                            <li class="list-group-item"> <i class="fa-solid fa-stopwatch"></i> Czas trwania: ${data[i].time} min</li>
+                            <li class="list-group-item"> <i class="fa-solid fa-dollar-sign"></i> Cena: ${data[i].price}zł</li>
+                          </ul>
+                          <br>
+                        <a href="#" class="btn btn-primary">Dodaj do koszyka</a>
                 </div>
         </div>
   </div>`
   );
 };
 
-//pobieranie zabiegow
+//pobieranie zabiegow z JSONa
 const database = fetch("database.json")
   .then((result) => result.json())
   .then(function (data) {
     console.log(data.treatments);
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < data.treatments.length; i++) {
       cards(data.treatments, i);
     }
   })
   .catch((err) => console.warn(err));
+//koniec pobierania
